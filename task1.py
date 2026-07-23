40732336468421c1e4f3a811d1085f95d0996deb
princesaini-077 <princesaini.is140@gmail.com>
2026-07-23 23:17:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num21
+if num2 != 0:
+    division=num1/num2
+print("addition",addition)
+print("subtraction",subtraction)
+print("multiplication",multiplication)
+print("division",division)
---
+++ task2.py
@@ -0,0 +1,6 @@
+first_name=input("Enter your first name: ").strip()
+last_name=input("Enter your last name: ").strip()
+
+full_name=first_name+" "+last_name
+
+print(f"\nhello, {full_name}! wellcome to the python program")
